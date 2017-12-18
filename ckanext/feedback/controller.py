a3e953f965491cbc66f56b94c3a23733a1ec76b5
Alberto Miedes Garcés <albertomg994@gmail.com>
2017-12-18 17:55:08 +0100

    Adapt extension to our needs

--- ckanext/feedback/controller.py
+++ ckanext/feedback/controller.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+
 import smtplib
 import os
+
 import ckan.plugins as p
+import ckan.plugins.toolkit as t
+
 import pylons.config as config
 
 from ckan.common import request
+from ckan.common import c
 from ckan.lib.base import BaseController
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
@@ -11,23 +17,37 @@ from email.MIMEText import MIMEText
 from email.Utils import COMMASPACE, formatdate
 from email import Encoders
 
-class FeedbackController(BaseController):
-  def feedback(self):
-    return p.toolkit.render('feedbackForm.html')
 
-  def feedbackProv(self):
-   # assert type(['maulik.kamdar@insight-centre.org'])==list
-    requestHandler = config.get('ckan.feedback.request_email', "maulik.kamdar@insight-centre.org") # this should read from config file or send email to default
-    senderEmail = config.get('ckan.feedback.sender_email', "admin@system.com") # this should read from config file or send email from default
-    msg = MIMEMultipart()
-    msg['From'] = senderEmail
-    msg['To'] = requestHandler
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = "Dataset Request for " + request.params['datasetName']
-    
-    dataRequest = "User Name: " + request.params['name'] + "\nUser Email: " + request.params['email'] + "\nOrganization Name: " + request.params['organization'] + "\nDataset Name: " + request.params['datasetName'] + "\nDataset Description: " + request.params['description']
-    msg.attach( MIMEText(dataRequest) )
-    smtp = smtplib.SMTP('localhost',25)
-    smtp.sendmail(senderEmail, requestHandler, msg.as_string() )
-    smtp.close()
-    return p.toolkit.render('feedbackProv.html')
+class FeedbackController(BaseController):
+
+    def feedback(self):
+        return p.toolkit.render('feedbackForm.html')
+
+    def feedbackProv(self):
+        current_user = t.get_action('user_show')({}, { 'id': c.user })
+        request_sender_name = current_user['display_name']
+
+        # build message
+
+        receiver_email = config.get('ckan.feedback.receiver_email')
+        sender_email   = config.get('ckan.feedback.sender_email')
+
+        msg = MIMEMultipart()
+        msg['From']    = sender_email
+        msg['To']      = receiver_email
+        msg['Date']    = formatdate(localtime=True)
+        msg['Subject'] = 'Nueva petición de datos recibida'
+
+        # TODO: solve encoding problems
+        # email_body = 'Nombre de usuario: ' + request_sender_name.encode('utf-8') + "\nPetición: ".decode('utf-8') + request.params['data_request_description']
+        email_body = 'Email body placeholder'
+
+        msg.attach(MIMEText(email_body))
+
+        # send email
+
+        smtp_server = smtplib.SMTP('localhost', 25)
+        smtp_server.sendmail(sender_email, receiver_email, msg.as_string() )
+        smtp_server.close()
+
+        return p.toolkit.render('feedbackProv.html')
--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
-class FeedbackPlugin(plugins.SingletonPlugin):
+from ckan.lib.plugins import DefaultTranslation
+
+
+class FeedbackPlugin(plugins.SingletonPlugin, DefaultTranslation):
+
   plugins.implements(plugins.IConfigurer)
   plugins.implements(plugins.IRoutes, inherit=True)
+  plugins.implements(plugins.ITranslation)
 
   def update_config(self,config):
     toolkit.add_template_directory(config, 'templates')
@@ -11,5 +18,5 @@ class FeedbackPlugin(plugins.SingletonPlugin):
   def before_map(self, m):
     m.connect('feedback', '/feedback', controller='ckanext.feedback.controller:FeedbackController', action='feedback')
     m.connect('feedbackProv', '/feedbackProv', controller='ckanext.feedback.controller:FeedbackController', action='feedbackProv')
-    return m
 
+    return m
